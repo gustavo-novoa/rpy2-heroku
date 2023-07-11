86e023fa90940a3e7bbd50b49802afe6dc1987b9
Jiwon Kim <40426834+j1wonkim@users.noreply.github.com>
2023-07-10 23:43:12 -0400

    Update diag.py

--- diag.py
+++ diag.py
@@ -53,9 +53,9 @@ class DiagResource(object):
                                                 , n.tr = 5
                                                 , tr.names = c("control", "0shaming", "0silence", "1shaming", "2shaming")
                                                 , assg.prob = c(1/5, 1/5, 1/5, 1/5, 1/5)
-                                                , assg.prob.method = "ktimes"
-                                                , assg.prob.kfac = 2
-                                                , distance = "mahalanobis"
+                                                #, assg.prob.method = "ktimes"
+                                                #, assg.prob.kfac = 2
+                                                #, distance = "mahalanobis"
                                                 , exact.vals = exact_va
                                                 , seed = 19930531
                                                 , file.name = session)
